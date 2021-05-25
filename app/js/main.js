5313536a5d7c8b5ce8714d549e0c36ea35ef8161
qamorozov <andr.morozov96@gmail.com>
2021-05-25 17:00:21 +0300

    added adaptive for tabs and section training

--- app/js/main.js
+++ app/js/main.js
@@ -101,33 +101,6 @@ const getScrollBarWidth = () => {
 // === / POPUP ===
 
 // === TABS ===
-// document.querySelectorAll('.tab-content').forEach(function(tabContent) {
-
-//     const tabsBtn = tabContent.querySelectorAll('.tab-list');
-//     const tabsItems = tabContent.querySelectorAll('.tab-item');
-
-//     tabsBtn.forEach(onTabClick);
-
-//     document.querySelector('.tab-list').click();
-
-//     function onTabClick(item) {
-//         item.addEventListener('click', function() {
-//             let currentBtn = item;
-//             let tabId = currentBtn.getAttribute("data-tab");
-//             let currentTab = tabContent.querySelector(tabId);
-//             if (!currentBtn.classList.contains('tab--active')) {
-//                 tabsBtn.forEach(function(item) {
-//                     item.classList.remove('tab--active');
-//                 });
-//                 tabsItems.forEach(function(item) {
-//                     item.classList.remove('tab-content--active');
-//                 });
-//             }
-//             currentBtn.classList.add('tab--active');
-//             currentTab.classList.add('tab-content--active');
-//         });
-//     };
-// });
 
 // === / TABS ===
 
